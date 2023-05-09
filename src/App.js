f1cfc73d36bb3aa12ee3df51798e2212065f1629
cclabdis <cclabdis@gmail.com>
2023-05-09 18:53:17 -0300

    dripburgurt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import HomePage from "./pages/HomePage/HomePage";
+import styled from "styled-components"
+import Header from "./components/Header/Header";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
+
+  const [habilitado, setHabilitado] = React.useState(false)
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+
+          <Route path='/' element={
+            <HomePage
+              habilitado={habilitado}
+              setHabilitado={setHabilitado}
+              
+             />} />
+                    
+          <Route path="/cart" element={
+            <Cart
+              habilitado={habilitado}
+              setHabilitado={setHabilitado}
+            />} />
+         
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  overflow: hidden;
+`
 
 export default App;
